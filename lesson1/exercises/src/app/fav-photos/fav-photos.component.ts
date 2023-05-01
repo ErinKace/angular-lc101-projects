827a375b17dc842b65c2f36e390d4cf034666442
ErinKace <erinkacerovskis@gmail.com>
2023-05-01 08:48:35 -0500

    completed part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image1 = '../assets/ThreeRoadsPosterColor.jpg';
+  image2 = '../assets/PrincessCharmingCharacters1.jpg';
   image3 = '';
 
   constructor() { }
