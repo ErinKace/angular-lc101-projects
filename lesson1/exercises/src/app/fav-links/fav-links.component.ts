50f9b4b58d1a33367b586f8c6c18399f76d63aaa
ErinKace <erinkacerovskis@gmail.com>
2023-05-04 20:08:53 -0500

    Stopped just before 30.3.2

--- lesson1/exercises/src/app/fav-links/fav-links.component.ts
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-links.component.css']
 })
 export class FavLinksComponent implements OnInit {
-  favLinks:string[] = ['http://www.ladyknightillustrations.com/','https://critters.org/', 'https://media3.giphy.com/media/Dh5q0sShxgp13DwrvG/giphy.gif?cid=ecf05e476j8zcygzcl0uhabitloiwlcvg5h4n0jm9yrsgd4b&ep=v1_gifs_search&rid=giphy.gif&ct=g'];
+  favLinks:string[] = ['http://www.ladyknightillustrations.com/','https://critters.org/'];
 
   constructor() { }
 
