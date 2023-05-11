533047b7b17757d7f8d02cc3286c6aba865f3f9a
ErinKace <erinkacerovskis@gmail.com>
2023-05-11 15:28:30 -0500

    Did lesson 3 examples

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -8,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
    inactive: boolean = false;
+   silverInactive: boolean = false;
+   copperInactive: boolean = false;
+   deactive: boolean = false;
+   moveButton: boolean = false;
 
    constructor() { }
 
    ngOnInit() { }
+
+  activateButtons() {
+      this.inactive = false;
+      this.silverInactive = false;
+      this.copperInactive = false;
+   }
 
 }
